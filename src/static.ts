b58b0823453fcee915a7a3381e8b95137b1717e1
islamemon59 <islamemon59@gmail.com>
2025-11-14 15:00:14 +0600

    static method with typescript

---
+++ src/static.ts
@@ -0,0 +1,27 @@
+class Counter {
+  static count: number = 0;
+  /// static premium when we call static we can't use this 
+  static increment() {
+    return (Counter.count = Counter.count + 1);
+  }
+
+  static decrement() {
+    return (Counter.count = Counter.count - 1);
+  }
+}
+
+const count1 = new Counter();
+const count2 = new Counter();
+
+// console.log(count1.increment()); // come from different memory
+// console.log(count1.increment());
+// console.log(count2.increment());// come from different memory
+// console.log(count2.increment());
+
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());  // static call with Object not instance
+console.log(Counter.increment());
+console.log(Counter.increment());  // come from all same memory 
+console.log(Counter.increment());
+console.log(Counter.increment());
