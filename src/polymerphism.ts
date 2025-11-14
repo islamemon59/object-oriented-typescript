ae3eb2c4e509fba2934f7094d53be16d644b2ec3
islamemon59 <islamemon59@gmail.com>
2025-11-14 19:08:27 +0600

    polymorphism with typescript

---
+++ src/polymerphism.ts
@@ -0,0 +1,73 @@
+// Polymorphism -> bohurupi
+
+class Person {
+  getSleep() {
+    console.log(`This is a normal person. He sleep everyday minimum 10 hour`);
+  }
+}
+
+class Student extends Person {
+  getSleep() {
+    console.log(`This is a student and he sleep every day total 8 hour`);
+  }
+}
+
+class NextLevelDeveloper extends Person {
+  getSleep() {
+    console.log(`This is next level developer`);
+  }
+}
+
+const getSleepHour = (param: Person) => {
+  param.getSleep();
+};
+
+const person1 = new Person();
+const person2 = new Student(); // polymorphism
+const person3 = new NextLevelDeveloper();
+
+getSleepHour(person3);
+
+//another example about polymorphism
+
+class Shape {
+  getArea(): number {
+    return 0;
+  }
+}
+
+class Circle extends Shape{
+    radius: number;
+    constructor(radius: number){
+        super()
+        this.radius = radius
+    }
+    getArea(): number{
+    return Math.PI* this.radius * this.radius;
+    }
+}
+
+class Rectangle extends Shape{
+    height: number;
+    width: number;
+
+    constructor(height: number, width: number){
+        super()
+        this.height = height;
+        this.width = width;
+    }
+    getArea(): number {
+        return this.height * this.width;
+    }
+}
+
+const getArea = (param: Shape) => {
+console.log(param.getArea());
+}
+
+const shape1 = new Shape()
+const shape2 = new Circle(10)
+const shape3 = new Rectangle(10, 20)
+
+
+getArea(shape3)
