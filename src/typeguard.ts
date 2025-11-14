d95ca4457bd1b8cb8bd52d302f5f4877d3fc4c68
islamemon59 <islamemon59@gmail.com>
2025-11-14 10:09:01 +0600

    typeguard typenarrow with type script

---
+++ src/typeguard.ts
@@ -0,0 +1,36 @@
+// type guard / type narrowing
+
+//type guard
+type AquaNumeric = number | string;
+const add = (num1: AquaNumeric, num2: AquaNumeric) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    num1.toString() + num2.toString();
+  }
+};
+
+add(2, 2);
+add(2, "2");
+add("2", 2);
+
+// in guard
+
+type NormalUser = {
+  name: string;
+};
+
+type NormalAdmin = {
+  name: string;
+  role: string;
+};
+
+const getUserInfo = (user: NormalUser | NormalAdmin) => {
+  if ("role" in user) {
+    console.log(`This ${user.name} is role is ${user.role}`);
+  } else {
+    console.log(`This ${user.name}`);
+  }
+};
+
+getUserInfo({ name: "Normal User", role: "admin" });
